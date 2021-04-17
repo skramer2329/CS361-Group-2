05ecc56f4d705d10e8eab51c095cdab3fa9d47a3
bennett schuster <schust87@uwm.edu>
2021-04-16 21:05:08 -0500

    Course and User models - mostly done

--- ourapp/models.py
+++ ourapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    address = models.CharField(max_length=40)
+    email = models.EmailField(max_length=30)
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=20)
+    course_number = models.IntegerField()
+    instructor = models.ForeignKey(User)
+    ta_list = []
