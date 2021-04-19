75783db7f907d249709ec5f44473943f26385233
bennett schuster <schust87@uwm.edu>
2021-04-19 10:33:58 -0500

    Made Account an abstract class

--- ourapp/models.py
+++ ourapp/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+import abc
 
 # Create your models here.
 
 
-class Account(models.Model):
+class Account(abc):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     username = models.CharField(max_length=20)
