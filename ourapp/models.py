6e4c3dbcd42ba615f745e40102ca74640fc131c6
bennett schuster <schust87@uwm.edu>
2021-04-19 10:19:04 -0500

    Creating models for each of the 3 user types

--- ourapp/models.py
+++ ourapp/models.py
@@ -2,17 +2,38 @@ from django.db import models
 
 # Create your models here.
 
-class User(models.Model):
+
+class Account(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
     address = models.CharField(max_length=40)
     email = models.EmailField(max_length=30)
+    phone_number = models.CharField(max_length=15)
 
 
 class Course(models.Model):
     name = models.CharField(max_length=20)
     course_number = models.IntegerField()
-    instructor = models.ForeignKey(User)
-    ta_list = []
+    instructor = models.ForeignKey(Account)
+    ta_list = models.ManyToManyField(Account)
+    time = models.DateTimeField()
+
+
+class Section(models.Model):
+    course = models.ForeignKey(Course)
+
+
+class Supervisor(Account):
+    pass
+
+
+class Instructor(Account):
+    courses = models.ManyToOneRel(Course)
+    sections = models.ManyToOneRel(Section)
+
+
+class Ta(Account):
+    courses = models.ManyToManyField(Course)
+    sections = models.ManyToOneRel(Section)
