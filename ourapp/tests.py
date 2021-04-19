df726b61c1423fcbe1571bcbb6cafdb1f861125c
decle <declee28@uwm.edu>
2021-04-18 19:14:12 -0500

    Checking to see if my comment shows up in my branch when I commit and push

--- ourapp/tests.py
+++ ourapp/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
-
+from django.test import TestCase, Client
+from models import User
 # Create your tests here.
+
+class TestLoginSuccess(TestCase):
+    monkey = None
+    userOne = None
+
+    def setUp(self):
+        self.monkey = Client()
+        temp = User(username="userOne",password="userOne")
+        temp.save()
+
+    def test_correct_user_password_logs_in_successfully(self):
+        resp = self.monkey.post("/", {"name":"userOne","password":"userOne"}, follow=True)
+
--- ourapp/views.py
+++ ourapp/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
-
+from django.views import View
+from .models import User
 # Create your views here.
-#Checking to see if this makes it to my Github branch when I check.
+class Login(View):
+    def get(self, request):
+        return render(request, "login.html", {})
+
+    def post(self, request):
+        noSuchUser = False
+        badPassword = False
+        try:
+            m = User.objects.get(name=request.POST['uname'])
+            badPassword = (m.password != request.POST['psw'])
+        except:
+            noSuchUser = True
+        if noSuchUser:
+            return render(request, "home.html", {"message": "The username that you used does not exist. Please retry."})
+        elif badPassword:
+            return render(request, "home.html", {"message": "The password that you entered is not correct.  Please retry."})
+        else:
+            request.session["name"] = m.name
+            return redirect("/things/")
+
