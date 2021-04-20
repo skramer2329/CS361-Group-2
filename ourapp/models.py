7f26a7e29d42ceea919ea709ab49b2ed28a12492
bennett schuster <schust87@uwm.edu>
2021-04-19 22:39:17 -0500

    Messed around with models some more

--- ourapp/models.py
+++ ourapp/models.py
@@ -1,39 +1,41 @@
 from django.db import models
 import abc
-
+from .models import Instructor, Supervisor, Ta, Section, Course
 # Create your models here.
 
 
 class User(abc):
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
-    address = models.CharField(max_length=40)
-    email = models.EmailField(max_length=30)
+    first_name = models.CharField()
+    last_name = models.CharField()
+    password = models.CharField()
+    address = models.CharField()
+    email = models.EmailField()
     phone_number = models.CharField(max_length=15)
 
 
+class Instructor(User, models.Model):
+    courses = models.ManyToOneRel(Course)
+    sections = models.ManyToOneRel(Section)
+
+
+class Supervisor(User, models.Model):
+    pass
+
+
+class Ta(User, models.Model):
+    courses = models.ManyToManyField(Course)
+    sections = models.ManyToOneRel(Section)
+
+
 class Course(models.Model):
-    name = models.CharField(max_length=20)
-    course_number = models.IntegerField()
-    instructor = models.ForeignKey(Account)
-    ta_list = models.ManyToManyField(Account)
+    name = models.CharField()
+    number = models.IntegerField()
+    instructor = models.ForeignKey(Instructor)
+    ta_list = models.ManyToOneRel(Ta)
     time = models.DateTimeField()
 
 
 class Section(models.Model):
     course = models.ForeignKey(Course)
+    number = models.IntegerField()
 
-
-class Supervisor(Account):
-    pass
-
-
-class Instructor(Account):
-    courses = models.ManyToOneRel(Course)
-    sections = models.ManyToOneRel(Section)
-
-
-class Ta(Account):
-    courses = models.ManyToManyField(Course)
-    sections = models.ManyToOneRel(Section)
