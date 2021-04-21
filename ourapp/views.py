c8da4b4a8bb47d3dcdb2f1c660755ec0a51ffd44
bennett schuster <schust87@uwm.edu>
2021-04-20 20:55:38 -0500

    Account editing view should be nearly complete

--- ourapp/views.py
+++ ourapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from .models import User, Instructor
+from .models import User, Instructor, Supervisor, Ta, Course, Section
 # Create your views here.
 
 
@@ -17,9 +17,44 @@ class Accounts(View):
 class CreateAccounts(View):
     def creation(self, request):
         # We will have to change this because we cannot instantiate User objects (User is abstract)
-        a = User(email=request.POST['email'], password=request.POST['password'], first_name=request.POST['first_name'],
-                 last_name=request.POST['last_name'], address=request.POST['address'],
-                 phone_number=request.POST['phone_number'])
+        email=request.POST['email']
+        password=request.POST['password']
+        first_name=request.POST['first_name']
+        last_name=request.POST['last_name']
+        address=request.POST['address']
+        phone_number=request.POST['phone_number']
+        role=request.POST['role']
+
+        user_exists = False
+        instructors = Instructor.objects.all()
+        tas = Ta.objects.all()
+        supervisors = Supervisor.objects.all()
+        accounts = []
+        for i in instructors:
+            accounts.append(i)
+        for i in tas:
+            accounts.append(i)
+        for i in supervisors:
+            accounts.append(i)
+
+        for i in accounts:
+            if i.email == email:
+                return render(request, "account.html", {"accounts": accounts, "message": "email already exists"})
+
+        if role == 'Instructor':
+            a = Instructor.objects.create(email=email, password=password, first_name=first_name, last_name=last_name,
+                                          address=address, phone_number=phone_number)
+
+        elif role == 'Supervisor':
+            a = Supervisor.objects.create(email=email, password=password, first_name=first_name, last_name=last_name,
+                                          address=address, phone_number=phone_number)
+
+        else: #Ta
+            a = Ta.objects.create(email=email, password=password, first_name=first_name, last_name=last_name,
+                                          address=address, phone_number=phone_number)
+
         a.save()
-        accounts = list(User.objects)
+
+        accounts.append(a)
+
         return render(request, "account.html", {"accounts": accounts})
