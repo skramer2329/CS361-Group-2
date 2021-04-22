cbc8127bce4ca2e2ffc7a65d063cd1dc646f90b8
bennett schuster <schust87@uwm.edu>
2021-04-22 16:59:32 -0500

    Course changed to MyCourse

--- ourapp/admin.py
+++ ourapp/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import  Course, Section, MyUser
+from .models import  MyCourse, Section, MyUser
 
 # Register your models here.
 
-admin.site.register(Course)
+admin.site.register(MyCourse)
 admin.site.register(MyUser)
 admin.site.register(Section)
